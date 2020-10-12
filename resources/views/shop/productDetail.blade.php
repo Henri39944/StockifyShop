5d81ae71c45e635e7a43a00d798592915c7aea6f
henribunt <bunthenri@gmail.com>
2020-10-12 09:04:08 +0200

    breadcrumbs pfotos

--- resources/views/shop/productDetail.blade.php
+++ resources/views/shop/productDetail.blade.php
@@ -10,7 +10,23 @@
             <div class="col-md-12 col-sm-12">
                 <div class="row">
                     <div class="col-md-6">
-                        <img src="{{ asset('/uploads/images/collection/arrivals1.png') }}" alt="new-arrivals images">
+                        <div class="large-preview-parent">
+                            <div class="large-preview">
+                                <img src="{{ asset('/uploads/images/collection/arrivals1.png') }}" alt="new-arrivals images">
+                            </div>
+                        </div>
+                        <div class="carousel_small_picture_preview_webshop">
+                            <ul class="image-list">
+                                <li class="image-item"><img src="{{ asset('/uploads/images/collection/arrivals9.png') }}" alt="new-arrivals images"></li>
+                                <li class="image-item"><img src="{{ asset('/uploads/images/collection/arrivals10.png') }}" alt="new-arrivals images"></li>
+                                <li class="image-item"><img src="{{ asset('/uploads/images/collection/arrivals11.png') }}" alt="new-arrivals images"></li>
+                                <li class="image-item"><img src="{{ asset('/uploads/images/collection/arrivals12.png') }}" alt="new-arrivals images"></li>
+                                <li class="image-item"><img src="{{ asset('/uploads/images/collection/arrivals13.png') }}" alt="new-arrivals images"></li>
+                                <li class="image-item"><img src="{{ asset('/uploads/images/collection/arrivals14.png') }}" alt="new-arrivals images"></li>
+                            </ul>
+                            <a class="btnPrevious_carousel">terug</a>
+                            <a class="btnNext_carousel">volgende</a>
+                        </div>
                     </div>
                     <div class="col-md-6">
                         <h4>lorem ipsum</h4>
